08442c4a929617fb2a1da67e77b8cc7e83ef0fdf
25584\PC_USER <kaito.20070626s@gmail.com>
2026-06-29 11:38:44 +0900

    初回コミットです！！

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private int N = 10;
+
+    private string name1 = "Nishio";
+    private string name2 = "FANZA";
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
